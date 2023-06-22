494527cb19e433e8f566fa59ea9c4d6d2daf5b09
GDcocos12 <124127742+GDcocos12@users.noreply.github.com>
2023-06-22 20:54:50 +0400

    Source Code

--- Source.cpp
+++ Source.cpp
@@ -1,51 +1,52 @@
 #include <iostream>
+#include <map>
 #include <string>
-#include <unordered_map>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 #include <fstream>
 #include <sstream>
 
-std::unordered_map<std::string, double> variables;
+std::map<std::string, double> variables;
 
 void handle_dclr(std::string variable_name, std::string data_type, std::string value) {
+    double val;
     if (data_type == "int") {
-        variables[variable_name] = std::stoi(value);
+        val = std::stoi(value);
     }
     else if (data_type == "float") {
-        variables[variable_name] = std::stof(value);
+        val = std::stod(value);
     }
+    variables[variable_name] = val;
 }
 
 void handle_opr(std::string result_variable, std::string arg1, std::string action, std::string arg2) {
-    double num1, num2;
-    if (variables.find(arg1) != variables.end()) {
-        num1 = variables[arg1];
+    double val1, val2;
+    if (variables.count(arg1)) {
+        val1 = variables[arg1];
     }
     else {
-        num1 = std::stod(arg1);
+        val1 = std::stod(arg1);
     }
-    if (variables.find(arg2) != variables.end()) {
-        num2 = variables[arg2];
+    if (variables.count(arg2)) {
+        val2 = variables[arg2];
     }
     else {
-        num2 = std::stod(arg2);
+        val2 = std::stod(arg2);
     }
     double result;
     if (action == "+") {
-        result = num1 + num2;
+        result = val1 + val2;
     }
     else if (action == "-") {
-        result = num1 - num2;
+        result = val1 - val2;
     }
     else if (action == "*") {
-        result = num1 * num2;
+        result = val1 * val2;
     }
     else if (action == "/") {
-        result = num1 / num2;
+        result = val1 / val2;
     }
     else if (action == "**") {
-        result = pow(num1, num2);
+        result = pow(val1, val2);
     }
     else {
         std::cout << "Invalid action" << std::endl;
@@ -58,7 +59,7 @@ void handle_print(std::string value) {
     if (value[0] == '"' && value[value.size() - 1] == '"') {
         std::cout << value.substr(1, value.size() - 2) << std::endl;
     }
-    else if (variables.find(value) != variables.end()) {
+    else if (variables.count(value)) {
         std::cout << variables[value] << std::endl;
     }
     else {
@@ -67,26 +68,28 @@ void handle_print(std::string value) {
 }
 
 void handle_rnd(std::string variable_name, std::string min_value, std::string max_value) {
-    double min, max;
-    if (variables.find(min_value) != variables.end()) {
-        min = variables[min_value];
+    double min_val, max_val;
+    if (variables.count(min_value)) {
+        min_val = variables[min_value];
     }
     else {
-        min = std::stod(min_value);
+        min_val = std::stoi(min_value);
     }
-    if (variables.find(max_value) != variables.end()) {
-        max = variables[max_value];
+    if (variables.count(max_value)) {
+        max_val = variables[max_value];
     }
     else {
-        max = std::stod(max_value);
+        max_val = std::stoi(max_value);
     }
-    srand(time(NULL));
-    double result = min + (max - min) * rand() / RAND_MAX;
-    variables[variable_name.substr(1, variable_name.size() - 2)] = result;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(min_val, max_val);
+    double result = dis(gen);
+    variables[variable_name.substr(1, variable_name.size() - 2)] = (int)result;
 }
 
 void handle_copy(std::string variable1, std::string variable2) {
-    if (variables.find(variable2) != variables.end()) {
+    if (variables.count(variable2)) {
         double value = variables[variable2];
         variables[variable1] = value;
     }
@@ -97,13 +100,13 @@ void handle_copy(std::string variable1, std::string variable2) {
 
 void handle_if(std::vector<std::string> tokens) {
     double arg1, arg2;
-    if (variables.find(tokens[1]) != variables.end()) {
+    if (variables.count(tokens[1])) {
         arg1 = variables[tokens[1]];
     }
     else {
         arg1 = std::stod(tokens[1]);
     }
-    if (variables.find(tokens[3]) != variables.end()) {
+    if (variables.count(tokens[3])) {
         arg2 = variables[tokens[3]];
     }
     else {
@@ -133,7 +136,8 @@ void handle_if(std::vector<std::string> tokens) {
         return;
     }
     if (condition) {
-        for (int i = 4; i < tokens.size(); i++) {
+        int i = 4;
+        while (i < tokens.size()) {
             if (tokens[i] == "END") {
                 break;
             }
@@ -152,32 +156,52 @@ void handle_if(std::vector<std::string> tokens) {
             else if (tokens[i] == "COPY") {
                 handle_copy(tokens[i + 1], tokens[i + 2]);
             }
+            else if (tokens[i] == "IF") {
+                std::vector<std::string> nested_tokens;
+                int nested_depth = 0;
+                for (int j = i; j < tokens.size(); j++) {
+                    if (tokens[j] == "IF") {
+                        nested_depth++;
+                    }
+                    else if (tokens[j] == "END") {
+                        nested_depth--;
+                        if (nested_depth == 0) {
+                            nested_tokens.push_back(tokens[j]);
+                            break;
+                        }
+                    }
+                    nested_tokens.push_back(tokens[j]);
+                }
+                handle_if(nested_tokens);
+                i += nested_tokens.size();
+            }
+            i++;
         }
     }
 }
 
 void handle_ord(std::vector<std::string> tokens) {
     std::string variable_name = tokens[1].substr(1, tokens[1].size() - 2);
-    char ch = tokens[2][1];
-    int ascii_code = static_cast<int>(ch);
+    char c = tokens[2][1];
+    int ascii_code = static_cast<int>(c);
     variables[variable_name] = ascii_code;
 }
 
 void handle_pow(std::string result_variable, std::string arg1, std::string arg2) {
-    double num1, num2;
-    if (variables.find(arg1) != variables.end()) {
-        num1 = variables[arg1];
+    double val1, val2;
+    if (variables.count(arg1)) {
+        val1 = variables[arg1];
     }
     else {
-        num1 = std::stod(arg1);
+        val1 = std::stod(arg1);
     }
-    if (variables.find(arg2) != variables.end()) {
-        num2 = variables[arg2];
+    if (variables.count(arg2)) {
+        val2 = variables[arg2];
     }
     else {
-        num2 = std::stod(arg2);
+        val2 = std::stod(arg2);
     }
-    double result = pow(num1, num2);
+    double result = pow(val1, val2);
     variables[result_variable.substr(1, result_variable.size() - 2)] = result;
 }
 
@@ -188,20 +212,31 @@ void handle_inp(std::string result_variable) {
     variables[result_variable.substr(1, result_variable.size() - 2)] = std::stod(user_input);
 }
 
-int main() {
-    std::cout << "Welcome to DUDOLIN INTERPRETER v1.0" << std::endl;
-    std::string filename;
-    std::cout << "Enter the file name: ";
-    std::cin >> filename;
+void execute_file(std::string filename) {
     std::ifstream file(filename);
     if (file.is_open()) {
         std::string command;
+        bool in_string = false;
         while (std::getline(file, command, ';')) {
             std::vector<std::string> tokens;
             std::istringstream iss(command);
             std::string token;
             while (iss >> token) {
-                tokens.push_back(token);
+                if (in_string) {
+                    tokens.back() += " " + token.substr(0, token.size() - 1);
+                    if (token.back() == '"') {
+                        in_string = false;
+                    }
+                }
+                else if (token.front() == '"') {
+                    tokens.push_back(token.substr(1));
+                    if (token.back() != '"') {
+                        in_string = true;
+                    }
+                }
+                else {
+                    tokens.push_back(token);
+                }
             }
             if (tokens[0] == "DCLR") {
                 handle_dclr(tokens[1], tokens[2], tokens[3]);
@@ -230,6 +265,92 @@ int main() {
             else if (tokens[0] == "INP") {
                 handle_inp(tokens[1]);
             }
+            else if (tokens[0] == "EXEC") {
+                if (tokens[1][0] == '{' && tokens[1].back() == '}') {
+                    std::string filename = tokens[1].substr(1, tokens[1].length() - 2);
+                    execute_file(filename);
+                }
+                else {
+                    std::cout << "Invalid file name format" << std::endl;
+                }
+            }
+            else if (tokens[0] == "END") {
+                break;
+            }
+        }
+        file.close();
+    }
+    else {
+        std::cout << "Unable to open file" << std::endl;
+    }
+}
+
+int main() {
+    std::cout << "Welcome to DUDOLIN INTERPRETER v1.1" << std::endl;
+    std::string filename;
+    std::cout << "Enter the file name: ";
+    std::cin >> filename;
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        std::string command;
+        bool in_string = false;
+        while (std::getline(file, command, ';')) {
+            std::vector<std::string> tokens;
+            std::istringstream iss(command);
+            std::string token;
+            while (iss >> token) {
+                if (in_string) {
+                    tokens.back() += " " + token.substr(0, token.size() - 1);
+                    if (token.back() == '"') {
+                        in_string = false;
+                    }
+                }
+                else if (token.front() == '"') {
+                    tokens.push_back(token.substr(1));
+                    if (token.back() != '"') {
+                        in_string = true;
+                    }
+                }
+                else {
+                    tokens.push_back(token);
+                }
+            }
+            if (tokens[0] == "DCLR") {
+                handle_dclr(tokens[1], tokens[2], tokens[3]);
+            }
+            else if (tokens[0] == "OPR") {
+                handle_opr(tokens[1], tokens[2], tokens[3], tokens[4]);
+            }
+            else if (tokens[0] == "PRINT") {
+                handle_print(tokens[1]);
+            }
+            else if (tokens[0] == "RND") {
+                handle_rnd(tokens[1], tokens[2], tokens[3]);
+            }
+            else if (tokens[0] == "IF") {
+                handle_if(tokens);
+            }
+            else if (tokens[0] == "COPY") {
+                handle_copy(tokens[1], tokens[2]);
+            }
+            else if (tokens[0] == "ORD") {
+                handle_ord(tokens);
+            }
+            else if (tokens[0] == "POW") {
+                handle_pow(tokens[1], tokens[2], tokens[3]);
+            }
+            else if (tokens[0] == "INP") {
+                handle_inp(tokens[1]);
+            }
+            else if (tokens[0] == "EXEC") {
+                if (tokens[1][0] == '{' && tokens[1].back() == '}') {
+                    std::string filename = tokens[1].substr(1, tokens[1].length() - 2);
+                    execute_file(filename);
+                }
+                else {
+                    std::cout << "Invalid file name format" << std::endl;
+                }
+            }
             else if (tokens[0] == "END") {
                 break;
             }
@@ -243,3 +364,4 @@ int main() {
     system("pause");
     return 0;
 }
+
